ef640c91ece825e8bfe20ed5d060129ed33accbe
Yumo Yuan <yyuan29@cmc.edu>
2026-02-26 23:32:30 -0800

    new

--- markdown_compiler/__init__.py
+++ markdown_compiler/__init__.py
@@ -193,19 +193,6 @@ def compile_lines(text):
         line = compile_links(line)
         new_lines.append(line)
 
-    if "</code>" in line:
-        parts = line.split("</code>", 1)
-        before = parts[0] + "</code>"
-        after = parts[1].lstrip()
-
-        if after:
-            new_lines.append(before)
-            new_lines.append(after)
-        else:
-            new_lines.append(line)
-    else:
-        new_lines.append(line)
-
     if in_paragraph:
         new_lines.append("</p>")
 
